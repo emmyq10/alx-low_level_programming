bdd0716cc0d7e34d396a4c8f52e7361a0cac244d
emmyq10 <emmatunde2001@gmail.com>
2023-05-19 09:45:37 -0700

    first task my self

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * Print_dlistint - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,2 @@
+0x17. C - Doubly linked lists
+this task is all obout doubly linked list
